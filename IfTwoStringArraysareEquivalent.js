af30d3ede1c63a61e6ce42033da37dc3b92bc666
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-12 17:09:23 +0530

    Check If Two String Arrays are Equivalent

---
+++ IfTwoStringArraysareEquivalent.js
@@ -0,0 +1,11 @@
+var arrayStringsAreEqual = function(word1, word2) {
+    
+    let x = word1.join("")
+    let y = word2.join("")
+    let ans = x==y
+    console.log(ans);
+
+};
+const word1 = ["assb", "c"], word2 = ["a", "bc"]
+arrayStringsAreEqual(word1,word2)
+
