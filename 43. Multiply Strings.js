1585b3ecc3ef09c1c202bf83b6a617e57b60dc5c
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-26 15:11:32 +0530

    43. Multiply Strings

---
+++ 43. Multiply Strings.js
@@ -0,0 +1,10 @@
+
+// let num1="2";
+// let nmu2= "32"
+// let ans = num1*nmu2
+// console.log(num1);
+
+var multiply = function(num1, num2) {
+  return  parseInt(num1)  * parseInt( num2)
+};
+console.log(multiply("25","61"));
