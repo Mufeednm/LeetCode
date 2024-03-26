22b45f095e21cf0b095c71295fd39930185ccf46
mufeednm <mufeedmusthafanm@gmail.com>
2024-03-26 15:29:43 +0530

    43. Multiply Strings by BigInt

--- returnoflength.js
+++ returnoflength.js
@@ -6,6 +6,6 @@ var argumentsLength = function(...args) {
     return args.length;
 
 };
+let  args = [{}, null, "3"]
 
-
- console.log(argumentsLength(1, 2, 3))
+ console.log(argumentsLength(...args))
